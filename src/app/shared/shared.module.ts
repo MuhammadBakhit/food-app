08d03e7ac64c171ce55f4317f4ed20d08c031262
Muhammad Bakhit <mohamedbakhetoiu@gmail.com>
2025-02-25 21:16:04 +0200

    init message

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SidebarComponent } from './sidebar/sidebar.component';
 import { NavbarComponent } from './navbar/navbar.component';
+import { RouterModule } from '@angular/router';
 
 
 
@@ -11,11 +12,13 @@ import { NavbarComponent } from './navbar/navbar.component';
     NavbarComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    RouterModule
   ],
   exports: [
     SidebarComponent,
-    NavbarComponent
+    NavbarComponent,
+    RouterModule
   ]
 })
 export class SharedModule { }
--- src/app/shared/sidebar/sidebar.component.ts
+++ src/app/shared/sidebar/sidebar.component.ts
@@ -1,10 +1,60 @@
 import { Component } from '@angular/core';
-
+interface IMenu{
+  title: string;
+  icon: string;
+  menuLink: string;
+  isActive: boolean
+}
 @Component({
   selector: 'app-sidebar',
   templateUrl: './sidebar.component.html',
   styleUrls: ['./sidebar.component.scss']
 })
 export class SidebarComponent {
+menu: IMenu[]=[
+{
+  title: 'home',
+  icon: 'fa-house-user',
+  menuLink: '/dashboard',
+  isActive: this.isAdmin() || this.isUser()
+},
+{
+  title: 'Users',
+  icon: 'fa-users',
+  menuLink: '/dashboard',
+  isActive: this.isAdmin()
+},
+{
+  title: 'Recipes',
+  icon: 'fa-braille',
+  menuLink: '/dashboard',
+  isActive: this.isAdmin()
+},
+{
+  title: 'User Recipes',
+  icon: 'fa-braille',
+  menuLink: '/dashboard',
+  isActive: this.isUser()
+},
+{
+  title: 'Categories',
+  icon: 'fa-list',
+  menuLink: '/dashboard',
+  isActive: this.isAdmin()
+},
+{
+  title: 'Favorites',
+  icon: 'fa-heart',
+  menuLink: '/dashboard',
+  isActive: this.isUser() 
+}
+];
 
+
+  isAdmin(){
+    return localStorage.getItem('role') === 'SuperAdmin'? true : false
+  }
+  isUser(){
+    return localStorage.getItem('role') === 'SystemUser'? true : false
+  }
 }
